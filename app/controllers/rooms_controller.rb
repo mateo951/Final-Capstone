80a86e9440b359abce6e0ef1a3ea4ccd92881192
ForHemer <foreste.hemerson@gmail.com>
2022-07-19 20:29:23 -0400

    Correct linter errors

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,5 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: %i[ show update destroy ]
+  before_action :set_room, only: %i[show update destroy]
 
   # GET /rooms
   def index
@@ -39,13 +39,14 @@ class RoomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_room
-      @room = Room.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def room_params
-      params.require(:room).permit(:name, :image_url, :price, :description, :surface)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_room
+    @room = Room.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def room_params
+    params.require(:room).permit(:name, :image_url, :price, :description, :surface)
+  end
 end
